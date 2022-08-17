911034a6119a03ae8d5b8ba016ff97b3e9a9169d
flavio.santana <9090flavio@gmail.com>
2022-08-17 15:01:12 -0300

    feat: endpoint handlers

--- src/main/java/io/javalin/HelloWorld.java
+++ src/main/java/io/javalin/HelloWorld.java
@@ -5,7 +5,18 @@ public class HelloWorld {
         Javalin app = Javalin.create().start(7080);
         app.get("/", ctx -> ctx.result("Bem Vindo ao JAVALIN"));
 
+        beforeHandlers(app);
+        endpointHandlers(app);
+    }
+
+    private static void beforeHandlers(Javalin app) {
         app.before((ctx) -> System.out.println("runs before all requests"));
         app.before("/path/*", ctx -> System.out.println("runs before all requests in /path"));
+    }
+
+    private static void endpointHandlers
+        (Javalin app) {
+        app.get("/path/output", ctx -> ctx.json("{\"message\":\"Hello World\"}"));
+        app.post("/input", ctx -> ctx.status(201));
     }
 }
