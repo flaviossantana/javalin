eeba4fb45cdfd9b7f40946c7095195a11ab37645
flavio.santana <9090flavio@gmail.com>
2022-08-17 14:54:02 -0300

    feat: before handlers

--- src/main/java/io/javalin/HelloWorld.java
+++ src/main/java/io/javalin/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7080);
         app.get("/", ctx -> ctx.result("Bem Vindo ao JAVALIN"));
+
+        app.before((ctx) -> System.out.println("runs before all requests"));
+        app.before("/path/*", ctx -> System.out.println("runs before all requests in /path"));
     }
 }
